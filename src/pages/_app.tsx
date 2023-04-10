24504215f1d66c9f138497212a3fa5a1da3731a2
bruno <brunotheodoro123@gmail.com>
2023-04-10 14:56:59 -0300

    refactor: melhorias no aplicativo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { theme } from '../styles/theme';
 
-import { PokemonProvider } from '../context/pokemonProvider';
+import { PokemonProvider } from '../context/PokemonProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
